a3c1f483f35ce84a1cad9dcc7e37a9a5b9946f59
SpeedyxBR <andrel.cilva57@gmail.com>
2025-10-24 10:28:58 -0300

    chore: implement user API endpoints and update server configuration

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ import { PostgresHelper } from './src/db/postgres/helper.js';
 
 const app = express();
 
-app.get('/', async (req, res) => {
+app.use(express.json());
+
+app.get('/api/users', async (req, res) => {
   try {
     const results = await PostgresHelper.query('SELECT * FROM users');
-    res.send(JSON.stringify(results));
+   res.send(json.stringify(results));
   } catch (error) {
     console.error('Erro ao executar query:', error);
     res.status(500).send('Erro interno do servidor');
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.post('/api/users', async (req, res) => {
+  console.log(req.body);
+  console.log(req.headers);
+  res.status(201).send('Usuário criado com sucesso');
 });
 
-export default app;
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
