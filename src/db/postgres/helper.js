0cd91775978859858b622b787a644278425f6f62
SpeedyxBR <andrel.cilva57@gmail.com>
2025-11-19 11:32:04 -0300

    fix(controllers):  spacing in error response object

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -22,3 +22,28 @@ export const PostgresHelper = {
         return results.rows
     },
 }
+
+export const badRequest = (errorMessage) => {
+    return {
+        statusCode: 400,
+        body: {
+            errorMessage,
+        },
+    }
+}
+
+export const created = (body) => {
+    return {
+        statusCode: 201,
+        body,
+    }
+}
+
+export const serverError = () => {
+    return {
+        statusCode: 500,
+        body: {
+            message: 'Internal server error',
+        },
+    }
+}
